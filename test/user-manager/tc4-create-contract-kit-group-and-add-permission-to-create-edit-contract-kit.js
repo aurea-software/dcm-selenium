cf296772d5cd1536713b97f37d408597a6f2533b
Linh Bui <Linh Bui>
2016-02-13 22:50:29 +0800

    new TC4 - User Manager - Create Contract Kit Group and Add Permission To Create / Edit Contract Kit

---
+++ test/user-manager/tc4-create-contract-kit-group-and-add-permission-to-create-edit-contract-kit.js
@@ -0,0 +1,118 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var assert = require("chai").assert;
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+var asserters = wd.asserters;
+
+var url = config.get("url");
+var username = config.get("adminUsername");
+var password = config.get("adminPassword");
+
+var common = require('../lib/common');
+
+var r = common.rand(4);
+var groupName = 'G' + r;
+
+console.log('r: ' + r);
+
+describe("/user-manager/tc4-create-contract-kit-group-and-add-permission-to-create-edit-contract-kit", function() {
+    this.timeout(60000);
+    var browser;
+
+    before(function (done) {
+        chaiAsPromised.transferPromiseness = wd.transferPromiseness;
+        browser = wd.promiseChainRemote(config.get("remote"));
+        common.configBrowser(browser, config.get("environment")).nodeify(done);
+    });
+
+    after(function (done) {
+        browser.quit().nodeify(done);
+    });
+
+    it("should login as admin", function (done) {
+        common.loginToUserManager(browser, url, username, password).nodeify(done);
+    });
+
+    it("should load user manager page", function(done) {
+        browser
+              .frame()
+              .frame('navbar')
+              .elementById('UserManager').click()
+              .nodeify(done);
+    });
+
+    it("should load group page", function(done) {
+        browser
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .elementByCss('#Search_UserManager_Users_Main_primary_display_div button').click()
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .elementByLinkText('Search Groups').click()
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .elementById('Button_UserManager_Groups_Main_CreateGroup').click()
+            .nodeify(done);
+    });
+
+    it("should create group", function(done) {
+        common.createGroup(browser, 'cacheframe1', groupName)
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .elementByCss('table[name=Grid_UserManager_Groups_Main] tbody tr:nth-child(1) td:nth-child(1)').text()
+            .should.eventually.become(groupName.toUpperCase())
+            .notify(done);
+    });
+
+    it("should load permission page", function(done) {
+        browser
+            .frame()
+            .frame('sidebar')
+            .elementById('Tab_UserManager_Groups_Main_AdditionalPermissions_link').click()
+            .nodeify(done);
+    });
+
+    it("should load edit permission page", function(done) {
+        browser
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .frame('component_iframe')
+            .elementById('Button_UserManager_Groups_Main_AdditionalPermissions_EditPermissions').click()
+            .nodeify(done);
+    });
+
+    it("should add permission", function(done) {
+        common.addPermission(browser, 'cacheframe1', 'ContractKitPropertyPage')
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('proppage')
+            .elementById('save').click()
+            .frame()
+            .frame('container')
+            .frame('cacheframe1')
+            .frame('subpage')
+            .frame('component_iframe')
+            .elementByCss('table[name=Grid_UserManager_Groups_Main_AdditionalPermissions] tbody tr:nth-child(1) td:nth-child(1)').text()
+            .should.eventually.become('CONTRACTKITPROPERTYPAGE')
+            .notify(done);
+    });
+
+});
