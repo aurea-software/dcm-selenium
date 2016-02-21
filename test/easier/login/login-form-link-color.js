c93225057cadc2c24bf71de219c5fde2f8ac142d
alexey-ernest <alexey.ernest@gmail.com>
2016-02-21 13:57:39 +0300

    ADCM-2807 Login Page - Login Form Link Color

---
+++ test/easier/login/login-form-link-color.js
@@ -0,0 +1,48 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+
+describe("login - form link color", function() {
+  this.timeout(30000);
+  var browser;
+ 
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+ 
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+ 
+  it("should be #23B1F7 rgba(35, 177, 247, 1)", function  (done) {
+    browser
+      .get(url)
+      .elementByCss('form[name=LoginForm] .checkbox a').getComputedCss('color')
+      .then(function(color) {
+        color.should.equal("rgba(35, 177, 247, 1)");
+      })
+      .nodeify(done);
+  });
+});
