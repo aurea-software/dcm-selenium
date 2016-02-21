f367363b280f46c23b26defee0120e3712056a80
alexey-ernest <alexey.ernest@gmail.com>
2016-02-21 14:07:41 +0300

    ADCM-2810 Login Page - Login Form Submit Button Font Size

---
+++ test/easier/login/login-form-submit-button-font-size.js
@@ -0,0 +1,48 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+
+describe("login - form submit button font size", function() {
+  this.timeout(30000);
+  var browser;
+ 
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+ 
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+ 
+  it("should be 17px", function  (done) {
+    browser
+      .get(url)
+      .elementByCss('form[name=LoginForm] button[type=submit]').getComputedCss('font-size')
+      .then(function(font) {
+        font.should.equal("17px");
+      })
+      .nodeify(done);
+  });
+});
