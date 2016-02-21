dc1d33920f4b9af1a3b39d0548b0e90b2dc067b3
alexey-ernest <alexey.ernest@gmail.com>
2016-02-21 12:56:13 +0300

    ADCM-2804 Login Page - Login Form Inputs Border Focus

---
+++ test/easier/login/login-form-inputs-border-focus.js
@@ -0,0 +1,56 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+
+describe("login - form inputs in focus border", function() {
+  this.timeout(30000);
+  var browser;
+ 
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+ 
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+ 
+  it("should be bottom 1px solid #28bd8b rgb(40, 189, 139)", function  (done) {
+    browser
+      .get(url)
+      .elementByCss('form[name=LoginForm] input[name=LOGINNAME]').type("").sleep(150) // wait for border color transition
+      .getComputedCss('border-bottom').then(function(border) {
+        border.should.equal("1px solid rgb(40, 189, 139)");
+      })
+      .elementByCss('form[name=LoginForm] input[name=PASSWORD]').type("").sleep(150) // wait for border color transition
+      .getComputedCss('border-bottom').then(function(border) {
+        border.should.equal("1px solid rgb(40, 189, 139)");
+      })
+      .elementByCss('.bootstrap-select button').click().sleep(150) // wait for border color transition
+      .getComputedCss('border-bottom').then(function(border) {
+        border.should.equal("1px solid rgb(40, 189, 139)");
+      })
+      .nodeify(done);
+  });
+});
