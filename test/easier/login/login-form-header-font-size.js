c5b15d3c1f6af8e077e7a3178773bca79f028f4e
alexey-ernest <alexey.ernest@gmail.com>
2016-02-21 14:16:25 +0300

    ADCM-2812 Login Page - Login Form Header Title Font Size

---
+++ test/easier/login/login-form-header-font-size.js
@@ -0,0 +1,47 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+
+describe("login - form header font size", function() {
+  this.timeout(30000);
+  var browser;
+ 
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+ 
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+ 
+  it("should be 25px", function  (done) {
+    browser
+      .get(url)
+      .elementByCss('.modal-title').getComputedCss('font-size').then(function(font) {
+        font.should.equal("25px");
+      })
+      .nodeify(done);
+  });
+});
