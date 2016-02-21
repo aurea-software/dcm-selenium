43f7c34bdf67d55b319de3b9cb4d5324f93e2bce
alexey-ernest <alexey.ernest@gmail.com>
2016-02-21 13:13:04 +0300

    ADCM-2805 Login Page - Login Form Submit Button Background

---
+++ test/easier/login/login-form-submit-button-bg-color.js
@@ -0,0 +1,48 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+
+describe("login - form submit button bg", function() {
+  this.timeout(30000);
+  var browser;
+ 
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+ 
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+ 
+  it("should be #28bd8b rgba(40, 189, 139, 1)", function  (done) {
+    browser
+      .get(url)
+      .elementByCss('form[name=LoginForm] button[type=submit]').getComputedCss('background-color')
+      .then(function(color) {
+        color.should.equal("rgba(40, 189, 139, 1)");
+      })
+      .nodeify(done);
+  });
+});
