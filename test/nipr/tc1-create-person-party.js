2ddccf0d2b702d16df820c4503e62a05e101de10
Linh Bui <Linh Bui>
2016-02-21 11:13:37 +0800

    new TC1 - NIPR - Create Person Party

---
+++ test/nipr/tc1-create-person-party.js
@@ -0,0 +1,65 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+var asserters = wd.asserters;
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../lib/common');
+var taxId = common.rand(5);
+
+describe("/nipr/tc1-create-person-party", function() {
+  this.timeout(60000);
+    var browser;
+
+    before(function (done) {
+      browser = wd.promiseChainRemote(config.get("remote"));
+      common.configBrowser(browser, config.get("environment")).nodeify(done);
+    });
+
+    after(function (done) {
+      browser
+        .quit()
+        .nodeify(done);
+    });
+
+    it("should login", function (done) {
+      common.login(browser, url, username, password).nodeify(done);
+    });
+
+    it("should load party page", function(done) {
+      browser
+        .frame('navbar')
+        .elementById('Party').click()
+        .nodeify(done);
+    });
+
+    it("should load create person page", function(done) {
+      browser
+        .frame()
+        .frame('container')
+        .frame('cacheframe0')
+        .frame('subpage')
+        .elementById('Button_Person_Main_NewPerson').click()
+        .nodeify(done);
+    });
+
+    it("should create person party", function(done) {
+        common.createPersonParty(browser, taxId, 'FN2', 'LN2', 'MN2', 'PN2', 'city2', 'Arizona', '1111', '2222')
+            .frame()
+            .frame('container')
+            .frame('cacheframe0')
+            .frame('subpage')
+            .elementById('Grid_Person_Main').text()
+            .should.eventually.include(taxId)
+            .notify(done);
+    });
+});
