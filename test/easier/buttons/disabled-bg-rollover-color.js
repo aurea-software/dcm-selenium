89fd78a2d6ba8b68591be2f7c98b5ed6e3d0e497
alexey-ernest <alexey.ernest@gmail.com>
2016-02-28 09:22:12 -0800

    ADCM-3233 Buttons - Disabled Background Rollover Color

---
+++ test/easier/buttons/disabled-bg-rollover-color.js
@@ -0,0 +1,57 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../../lib/common');
+
+describe("buttons - disabled bg rollover color", function() {
+  this.timeout(30000);
+  var browser;
+
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+
+  it("should be #B5B9BA rgba(181, 185, 186, 1)", function  (done) {
+    common.login(browser, url, username, password)
+      .frame('navbar')
+      .elementById('Party').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('subpage')
+      .elementByCss('.result-div .btn.pass').moveTo().sleep(150)
+      .getComputedCss('background-color').then(function(bgcolor) {
+        bgcolor.should.equal("rgba(181, 185, 186, 1)");
+      })
+      .nodeify(done);
+  });
+});
