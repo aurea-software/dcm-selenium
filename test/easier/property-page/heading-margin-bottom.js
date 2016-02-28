795033be90efe7c001a17aa02896dad2cbfbed21
alexey-ernest <alexey.ernest@gmail.com>
2016-02-28 11:10:40 -0800

    ADCM-3250 Property Page - Heading Margin Bottom

---
+++ test/easier/property-page/heading-margin-bottom.js
@@ -0,0 +1,62 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../../lib/common');
+
+describe("property page - heading margin bottom", function() {
+  this.timeout(30000);
+  var browser;
+
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+
+  it("should be 20px", function  (done) {
+    common.login(browser, url, username, password)
+      .frame('navbar')
+      .elementById('Party').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('subpage')
+      .elementById('Button_Person_Main_NewPerson').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('proppage')
+      .elementByCss('.heading').getComputedCss('margin-bottom')
+      .then(function(font) {
+        font.should.equal("20px");
+      })
+      .nodeify(done);
+  });
+});
