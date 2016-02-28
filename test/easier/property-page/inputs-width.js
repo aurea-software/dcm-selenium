6aaa6b7756f5ead59f69f7fa74cda6c342c42b62
alexey-ernest <alexey.ernest@gmail.com>
2016-02-28 12:05:19 -0800

    ADCM-3263 Property Page - Inputs Width

---
+++ test/easier/property-page/inputs-width.js
@@ -0,0 +1,67 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../../lib/common');
+
+describe("property page - inputs width", function() {
+  this.timeout(30000);
+  var browser;
+
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+
+  it("should be 100%", function  (done) {
+    var inputWidth = 0;
+    common.login(browser, url, username, password)
+      .frame('navbar')
+      .elementById('Party').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('subpage')
+      .elementById('Button_Person_Main_NewPerson').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('proppage')
+      .elementByCss('.form-group').getComputedCss('width')
+      .then(function(width) {
+        inputWidth = width;
+      })
+      .elementByCss('.form-group input.form-control').getComputedCss('width')
+      .then(function(width) {
+        width.should.equal(inputWidth);
+      })
+      .nodeify(done);
+  });
+});
