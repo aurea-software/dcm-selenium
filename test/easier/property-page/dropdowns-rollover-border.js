3a9f7eddde8d7fe5642e7925a7667eef9bc8b1e1
alexey-ernest <alexey.ernest@gmail.com>
2016-02-28 13:27:51 -0800

    ADCM-3274 Property Page - Dropdowns Rollover Border

---
+++ test/easier/property-page/dropdowns-rollover-border.js
@@ -0,0 +1,63 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../../lib/common');
+
+describe("property page - dropdowns rollover border", function() {
+  this.timeout(30000);
+  var browser;
+
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+
+  it("should be 1px solid #28BD8B rgb(40, 189, 139)", function  (done) {
+    common.login(browser, url, username, password)
+      .frame('navbar')
+      .elementById('Party').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('subpage')
+      .elementById('Button_Person_Main_NewPerson').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('proppage')
+      .elementByCss('.bootstrap-select button').moveTo().sleep(150)
+      .getComputedCss('border-bottom')
+      .then(function(border) {
+        border.should.equal("1px solid rgb(40, 189, 139)");
+      })
+      .nodeify(done);
+  });
+});
