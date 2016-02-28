0482a69172cc8f064faa3c8bd3c07285ca2fc46e
alexey-ernest <alexey.ernest@gmail.com>
2016-02-28 11:08:24 -0800

    ADCM-3249 Property Page - Heading Font Color

---
+++ test/easier/property-page/heading-font-color.js
@@ -0,0 +1,66 @@
+var config = require('nconf');
+config.file({file: './test/lib/config.json'});
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+var url = config.get("url");
+var username = config.get("username");
+var password = config.get("password");
+
+var common = require('../../lib/common');
+
+describe("property page - heading font color", function() {
+  this.timeout(30000);
+  var browser;
+
+  before(function (done) {
+    browser = wd.promiseChainRemote(config.get("remote")); 
+
+    // optional extra logging
+    browser.on('status', function(info) {
+      console.log(info);
+    });
+    browser.on('command', function(meth, path, data) {
+      console.log(' > ' + meth, path, data || '');
+    });
+
+    browser
+      .init(config.get("environment"))
+      .nodeify(done);  //same as : .then(function() { done(); });
+  });
+
+  after(function (done) {
+    browser
+      .quit()
+      .nodeify(done);
+  });
+
+  it("should be #313541 rgba(49, 53, 65, 1) for main text and #23B1F7 rgba(35, 177, 247, 1) for inner span", function  (done) {
+    common.login(browser, url, username, password)
+      .frame('navbar')
+      .elementById('Party').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('subpage')
+      .elementById('Button_Person_Main_NewPerson').click()
+      .frame()
+      .frame('container')
+      .frame('cacheframe0')
+      .frame('proppage')
+      .elementByCss('.heading').getComputedCss('color')
+      .then(function(font) {
+        font.should.equal("rgba(49, 53, 65, 1)");
+      })
+      .elementByCss('.heading span').getComputedCss('color')
+      .then(function(font) {
+        font.should.equal("rgba(35, 177, 247, 1)");
+      })
+      .nodeify(done);
+  });
+});
